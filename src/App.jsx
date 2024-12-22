e9c26fe7deededf3cbcbd7fc0e57b42f5eefad17
Francisco Jarquin <frankharkin9181@gmail.com>
2024-12-22 10:01:39 -0600

    Primera estancia (estatica) de SplashScreen terminada

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+//import './App.css'
+import SplashScreen from './components/SplashScreen.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
-    <>
+    <>{/*
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,6 +29,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      */}
+      <SplashScreen></SplashScreen>
     </>
   )
 }
---
+++ src/components/SplashScreen.jsx
@@ -0,0 +1,14 @@
+import logoDigui from './../assets/images/SplashScreen/Logo DIGUI 2.1.svg';
+import circle from './../assets/images/circle.svg';
+import './SplashScreen.css';
+
+function SplashScreen() {
+  return (
+    <div className='container'>
+      <p className='welcome-text'>Bienvenido</p>
+      <img src={logoDigui}></img>
+    </div>
+  );
+}
+
+export default SplashScreen;
